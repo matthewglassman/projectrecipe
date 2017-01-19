3af2e3feac3e6430cc07f07344f3bfc32be9294b
jlp0328 <jlp0328@gmail.com>
2017-01-19 11:48:31 -0500

    Updated JS file

--- js/appdemo.js
+++ js/appdemo.js
@@ -26,55 +26,55 @@ $('.slider').slider('prev');
 //      data: 'countries.json'
 // });
 
-var ingredients
+// var ingredients
 
 
 
 
-function populateYouTubeVideos(){
+// function populateYouTubeVideos(){
 
-var searchYouTube = "zucchini, cheese, beef";
+// var searchYouTube = "zucchini, cheese, beef";
 
- var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+searchYouTube+"&type=video&order=date&maxResults=10&key=AIzaSyBpu8hgnXbkqFVWrAvwRUEz7T13ii3I7WM";
-      console.log(searchYouTube);
-      console.log(queryURL);
+//  var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+searchYouTube+"&type=video&order=date&maxResults=10&key=AIzaSyBpu8hgnXbkqFVWrAvwRUEz7T13ii3I7WM";
+//       console.log(searchYouTube);
+//       console.log(queryURL);
 
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
+//       $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//       })
 
-      .done(function(response) {
-        var results = response.items;
-        console.log(results);
+//       .done(function(response) {
+//         var results = response.items;
+//         console.log(results);
 
-        for(var i = 0; i < 10; i++){
+//         for(var i = 0; i < 10; i++){
 
-          var videoId = results[i].id.videoId;
-          console.log(videoId);
+//           var videoId = results[i].id.videoId;
+//           console.log(videoId);
 
-          var videoSRC = "http://www.youtube.com/embed/"+videoId+"?enablejsapi=1&origin=http://example.com";
+//           var videoSRC = "http://www.youtube.com/embed/"+videoId+"?enablejsapi=1&origin=http://example.com";
 
-          var iFrameDiv = $("<div>");
-          iFrameDiv.attr("class", "container col s6");
+//           var iFrameDiv = $("<div>");
+//           iFrameDiv.attr("class", "container col s6");
 
-          var iFrame = $("<iframe></iframe>");
-          iFrame.attr("type", "text/html");
-          iFrame.attr("width", "320");
-          iFrame.attr("height", "195");
-          iFrame.attr("src", videoSRC);
-          iFrame.attr("frameborder", "0");
+//           var iFrame = $("<iframe></iframe>");
+//           iFrame.attr("type", "text/html");
+//           iFrame.attr("width", "320");
+//           iFrame.attr("height", "195");
+//           iFrame.attr("src", videoSRC);
+//           iFrame.attr("frameborder", "0");
 
-          $("#videoBank").append(iFrameDiv);
-          iFrameDiv.append(iFrame);
+//           $("#videoBank").append(iFrameDiv);
+//           iFrameDiv.append(iFrame);
 
-        }
+//         }
 
 
-});
-}
+// });
+// }
 
-populateYouTubeVideos();
+// populateYouTubeVideos();
 
 // Resolved
 
