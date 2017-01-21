cd9dd8ddf016ad980537d6193cd91a26d638a78c
jlp0328 <jlp0328@gmail.com>
2017-01-21 12:32:48 -0500

    updated html, video responsiveness

--- js/jamie.js
+++ js/jamie.js
@@ -16,9 +16,13 @@ $('.slider').slider('next');
 
 function populateYouTubeVideos(){
 
+//This variable will be a string that is pulled from flexdatalist and will be used to generate videos and recipes.
 var searchYouTube = "zucchini, cheese, beef";
 
- var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+searchYouTube+"&type=video&order=relevance&maxResults=10&key=AIzaSyBpu8hgnXbkqFVWrAvwRUEz7T13ii3I7WM";
+//This variable will be user input based on the number of videos they want to search for.
+var numRecipesToReturn = "10";
+
+ var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+searchYouTube+"&type=video&order=relevance&maxResults="+numRecipesToReturn+"&key=AIzaSyBpu8hgnXbkqFVWrAvwRUEz7T13ii3I7WM";
       console.log(searchYouTube);
       console.log(queryURL);
 
@@ -39,7 +43,9 @@ var searchYouTube = "zucchini, cheese, beef";
           var videoSRC = "http://www.youtube.com/embed/"+videoId+"?enablejsapi=1&origin=http://example.com";
 
           var iFrameDiv = $("<div>");
-          iFrameDiv.attr("class", "container col s6");
+          iFrameDiv.attr("class", "video-container")
+          ;
+          iFrameDiv.attr("id","iFrameDiv");
 
           var iFrame = $("<iframe></iframe>");
           iFrame.attr("type", "text/html");
